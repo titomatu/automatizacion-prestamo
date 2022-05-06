957629155e7f4aae1ba988cd4d21d3916b56a20b
Tito Andrés Maturana <tamatu@gmail.com>
2022-05-06 13:23:31 -0500

    First Commit

---
+++ src/main/java/edu/patrones/prestamo/PrestamoApplication.java
@@ -0,0 +1,13 @@
+package edu.patrones.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestamoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/patrones/prestamo/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.patrones.prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
