8c793795011c27bc1b726531b6a951922c448802
sabrosol <84605320+sabrosol@users.noreply.github.com>
2022-05-10 19:39:25 -0500

    version2

--- src/main/java/edu/patrones/prestamo/controller/GenerarOTPController.java
+++ src/main/java/edu/patrones/prestamo/controller/GenerarOTPController.java
@@ -3,6 +3,8 @@ package edu.patrones.prestamo.controller;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestBody;
